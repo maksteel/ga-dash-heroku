a0313113a2855027db9163d5d805b5e191e44fbb
maksteel <kukreja.m@gmail.com>
2021-01-09 22:44:05 +1300

    reading data

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly_express as px
+import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -8,8 +11,17 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
+df = pd.read_excel("https://github.com/chris1610/pbpython/blob/master/data/salesfunnel.xlsx?raw=True")
+mgr = df.Manager.unique()
+
+
 app.layout = html.Div([
-    html.H2('This is my first plotly Dash')
+    html.H2('This is my first plotly Dash'),
+    dcc.Dropdown(id = 'manager-dropdown',
+                options = [{'label': i, 'value' : i} for i in mgr],
+                value=None),
+    html.Div(id='selected-manager-div', children='selected manager is: '),
+    dcc.Graph(id='sales-funnel-bar-graph', figure=None)
 ])
 
 if __name__ == '__main__':
