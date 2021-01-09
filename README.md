08896fd523f2626487f0e65f90b3790fd6121360
maksteel <kukreja.m@gmail.com>
2021-01-09 21:58:49 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+1. Create heroku account
+2. Install heroku cli, git, anaconda
+3. clone this repo
+4. edit/ update environment.yml with your dependencies
+5. generate requirements.txt [pip freeze > requirements.txt]
+6. update app.py
+7. git add/commit/push heroku master
+8. heroku ps:scale web=1 [for the first time to start the web app]
---
+++ app.py
@@ -0,0 +1,16 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+server = app.server
+
+app.layout = html.Div([
+    html.H2('This is my first plotly Dash')
+])
+
+if __name__ == '__main__':
+    app.run_server()
